812792cb5a03f8f1ee5e9bd51e79cc7992a2e939
Eric Pradana <ericprd_@outlook.com>
2022-10-06 22:21:37 +0700

    connect to database

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const routes = require("./routes/routes.js");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const uri = process.env.DB_URI;
+
+try {
+  mongoose.connect(uri, () => {
+    console.log("connected to database");
+  });
+} catch (error) {
+  console.log(error);
+}
 
 require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
-app.get("/", (req,res) => {
-	res.send("helloWorld");
+app.get("/", (req, res) => {
+  res.send("helloWorld");
 });
 
 app.use("/api", routes);
 
 app.listen(3000, () => {
-	console.log("server is up");
+  console.log("server is up");
 });
